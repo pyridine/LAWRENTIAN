64dff48c5214f3826a4319b21859a25d990d70a9
pyyyrrd <pyridine@users.noreply.github.com>
2015-02-07 18:10:46 -0600

    translates ptokens to strings
    Function to translate permission token (int) to descriptive string.
    I'm just killing time tbh

--- LAWRENTIAN/LuD/PermissionDefinitions.h
+++ LAWRENTIAN/LuD/PermissionDefinitions.h
@@ -53,11 +53,12 @@ namespace PermissionDefinitions{
         EDIT_GRAPHIC = 21,
 
         /*Article Management*/
-        EDIT_ARTICLE_WORKSPACE = 22, //For creation, deletion, and editing article workspace (information).
+        EDIT_ARTICLE_WORKSPACE = 22, //For creation, deletion, and editing article workspaces.
 
         /*Idea Management*/
         VIEW_IDEAS = 23,
         EDIT_IDEAS = 24,
+        //Submitting ideas requires EDIT_ARTICLE_WORKSPACE permission.
 
         /*Section Permissions (to be used in tandem with edit/view permissions)*/
         SEC_FEATURES = 25,
@@ -69,14 +70,70 @@ namespace PermissionDefinitions{
 
     string translate(PToken a){
         switch(a){
+            case ADMIN:
+                return "Admin";
+            case VIEW_ADS:
+                return "View Ads";
+            case EDIT_ADS:
+                return "Edit Ads";
+            case VIEW_PERMISSIONS:
+                return "View Permissions";
+            case EDIT_PERMISSIONS:
+                return "View Permissions";
+            case VIEW_EMPLOYEE_INFO:
+                return "View Public Employee Information";
+            case VIEW_ALL_EMPLOYEE_INFO:
+                return "View Protected Employee Information";
+            case EDIT_EMPLOYEE_INFO:
+                return "Edit Employee Information";
+            case VIEW_TIMESHEETS:
+                return "View Timesheets";
+            case EDIT_TIMESHEETS:
+                return "Edit Timesheets";
+            case SEARCH_ARCHIVE:
+                return "Search the Archive";
+            case SUBMIT_TO_ARCHIVE:
+                return "Submit Issue to Archive";
+            case VIEW_SUBSCRIPTIONS:
+                return "View Subscriptions";
+            case EDIT_SUBSCRIPTIONS:
+                return "Edit Subscriptions";
+            case VIEW_CIRCULATIONS:
+                return "View Circulation Information";
+            case EDIT_CIRCULATIONS:
+                return "Edit Circulation Information";
+            case SUBMIT_COPY:
+                return "Submit Article Copy";
+            case EDIT_COPY:
+                return "Edit Article Copy";
+            case SUBMIT_PHOTO:
+                return "Submit Photos";
+            case EDIT_PHOTO:
+                return "Edit Photos";
+            case: SUBMIT_GRAPHIC:
+                return "Submit Illustrations";
+            case: EDIT_GRAPHIC:
+                return "Submit Illustrations";
+            case: EDIT_ARTICLE_WORKSPACE:
+                return "Create/Edit Article Workspaces";
+            case: VIEW_IDEAS:
+                return "View Ideas";
+            case: EDIT_IDEAS:
+                return "Edit Ideas";
+            case: SEC_FEATURES:
+                return "Features Section";
+            case: SEC_OPED:
+                return "Opinions & Editorials Section";
+            case: SEC_ARTS:
+                return "Arts Section";
+            case: SEC_SPORTS:
+                return "Sports Section";
+            case: SEC_VARIETY:
+                return "Variety Section";
             default:
-                return "lol";
+                return "ERROR :(";
         }
-        return "oops";
     }
-
 };
 
-
 #endif // DEFINITIONS
-
