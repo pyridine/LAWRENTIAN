39256bc28a58370d46255271210d5bbcf1ba87b6
Sanfer D'souza <sanferdsouza@gmail.com>
2015-02-12 21:20:51 -0600

    Example

--- LAWRENTIAN/LuD/main.cpp
+++ LAWRENTIAN/LuD/main.cpp
@@ -12,21 +12,29 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-
-    LoginWindow* l = new LoginWindow();
-    Client* c = new Client();
-    l->show();
-
-    if(c->connect()){
-        l->show();
-        l->initDB(c);
-
-    } else{
-        Alert::showAlert("!!FATAL ERROR!!","I couldn't connect to the database. \n This program will now terminate.");
-        return 1337;
-    }
-
-
-    return a.exec();
+    Sender sndr = Sender();
+    sndr.sendFile("C:/Users/lenovo/Dropbox/Hello.docx","Final.docx");
+    sndr.requestFile("C:/Users/Briggs 419 Server/Dropbox/Article/Document/Copy/Implement","C:/Programs/receivedfile.docx");
+    return 0;
 }
+
+//int main(int argc, char *argv[])
+//{
+//    QApplication a(argc, argv);
+
+//    LoginWindow* l = new LoginWindow();
+//    Client* c = new Client();
+//    l->show();
+
+//    if(c->connect()){
+//        l->show();
+//        l->initDB(c);
+
+//    } else{
+//        Alert::showAlert("!!FATAL ERROR!!","I couldn't connect to the database. \n This program will now terminate.");
+//        return 1337;
+//    }
+
+
+//    return a.exec();
+//}
