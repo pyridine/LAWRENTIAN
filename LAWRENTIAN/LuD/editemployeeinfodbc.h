d775babd3379b4fa91215fa0fdf219898322a346
egirsova <liza@girsovs.com>
2015-02-24 01:14:56 -0600

    Edit Employee UI Works with database

---
+++ LAWRENTIAN/LuD/editemployeeinfodbc.h
@@ -0,0 +1,12 @@
+#ifndef EDITEMPLOYEEINFODBC_H
+#define EDITEMPLOYEEINFODBC_H
+
+
+class EditEmployeeInfoDBC
+{
+public:
+    EditEmployeeInfoDBC();
+    ~EditEmployeeInfoDBC();
+};
+
+#endif // EDITEMPLOYEEINFODBC_H
