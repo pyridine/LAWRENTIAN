62f9d8e4694cc8295e00607f3cb6c93785f70eec
Sanfer D'souza <sanferdsouza@gmail.com>
2015-02-24 01:12:58 -0600

    main

--- LAWRENTIAN/LuD/main.cpp
+++ LAWRENTIAN/LuD/main.cpp
@@ -38,20 +38,18 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-//    LoginWindow* l = new LoginWindow();
-//    Client* c = new Client();
-//    l->show();
+    LoginWindow* l = new LoginWindow();
+    Client* c = new Client();
+    l->show();
 
-//    if(c->connect()){
-//        l->show();
-//        l->initDB(c);
+    if(c->connect()){
+        l->show();
+        l->initDB(c);
 
-//    } else{
-//        Alert::showAlert("!!FATAL ERROR!!","I couldn't connect to the database. \n This program will now terminate.");
-//        return 1337;
-//    }
+    } else{
+        Alert::showAlert("!!FATAL ERROR!!","I couldn't connect to the database. \n This program will now terminate.");
+        return 1337;
+    }
 
-    newArticleWorkspaceWindow *naww = new newArticleWorkspaceWindow;
-    naww->show();
     return a.exec();
 }
