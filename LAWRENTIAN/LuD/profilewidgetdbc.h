0ee59333dfd9d02eec59563271fd467786d54428
egirsova <liza@girsovs.com>
2015-03-02 18:52:33 -0600

    Changes in profile widget and writer timesheet. Also minor changes in the ui of naww

--- LAWRENTIAN/LuD/profilewidgetdbc.h
+++ LAWRENTIAN/LuD/profilewidgetdbc.h
@@ -12,6 +12,9 @@ public:
 
     string collectName(int luid);
     string collectTitle(int luid);
+    string collectArticleSection(int articleId);
+    string collectArticleTitle(int articleId);
+    vector<int> collectArticleIdForTimesheet(QDate currentDate, int writerId);
     vector<string> collectProbationApprovals(QDate currentDate);
     vector<int> collectWriterForTimesheet(QDate currentDate);
     void generateWriterTimesheet(int writerId, int articlesOnTime, int articlesLate, QDate issueDate);
